91d25d2d7db545e400a81470b2cc9100c5bec889
Angus Cheng <anguscheng389@gmail.com>
2019-06-25 21:00:36 +1000

    update

--- src/Array.java
+++ src/Array.java
@@ -71,7 +71,9 @@ class TwoDArray {
     }
 }
 
-
+/**
+ * Manually  allocate differing size dimensions.
+ */
 class TwoDAgain {
     public static void main(String args[]) {
         int twoD[] [] = new int[4] [];
@@ -91,6 +93,51 @@ class TwoDAgain {
         for (i=0; i<4; i++) {
             for (j=0; j<i+1; j++)
                 System.out.print(twoD[i] [j] + " ");
+            System.out.println();
+        }
+    }
+}
+
+/**
+ *  Initialise a two-dimensional array.
+ */
+class  Matrix {
+    public static void main(String args[]) {
+        double m[] [] = {
+                {0 * 0, 1 * 0, 2 * 0, 3 * 0},
+                {0 * 1, 1 * 1, 2 * 1, 3 * 1},
+                {0 * 2, 1 * 2, 2 * 2, 3 * 2},
+                {0 * 3, 1 * 3, 2 * 3, 3 * 3}
+        };
+        int i,j;
+
+        for(i=0; i<4; i++)  {
+        for (j = 0; j < 4; j++)
+            System.out.print(m[i][j] + " ");
+        System.out.println();
+    }
+    }
+}
+
+/**
+ * Three dimensional matrix
+ */
+class ThreeDMatrix {
+    public static void main(String args[]) {
+        int threeD[][][] = new int[3][4][5];
+        int i, j, k;
+
+        for (i = 0; i < 3; i++)
+            for (j = 0; j < 4; j++)
+                for (k = 0; k < 5; k++)
+                    threeD[i][j][k] = i * j * k;
+
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 4; j++) {
+                for (k = 0; k < 5; k++)
+                    System.out.print(threeD[i][j][k] + " ");
+                System.out.println();
+            }
             System.out.println();
         }
     }
