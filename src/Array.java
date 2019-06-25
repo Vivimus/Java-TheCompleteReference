f48e6b24ad611981cfef5a052d9a41760c779d4d
Angus Cheng <anguscheng389@gmail.com>
2019-06-25 17:02:14 +1000

    Added new array class

--- src/Array.java
+++ src/Array.java
@@ -49,6 +49,9 @@ class Average {
    }
 }
 
+/**
+ * Demonstrates a two-dimensional array.
+ */
 class TwoDArray {
     public static void main(String args[]) {
         int twoD[] [] = new int[4] [5];
@@ -62,6 +65,31 @@ class TwoDArray {
 
         for(i=0; i<4; i++) {
             for(j=0; j<5; j++)
+                System.out.print(twoD[i] [j] + " ");
+            System.out.println();
+        }
+    }
+}
+
+
+class TwoDAgain {
+    public static void main(String args[]) {
+        int twoD[] [] = new int[4] [];
+        twoD[0] = new int[1];
+        twoD[1] = new int[2];
+        twoD[2] = new int[3];
+        twoD[3] = new int[4];
+
+        int i, j, k = 0;
+
+        for (i=0; i<4; i++)
+            for (j=0; j<i+1; j++) {
+                twoD[i] [j] = k;
+                k++;
+            }
+
+        for (i=0; i<4; i++) {
+            for (j=0; j<i+1; j++)
                 System.out.print(twoD[i] [j] + " ");
             System.out.println();
         }
